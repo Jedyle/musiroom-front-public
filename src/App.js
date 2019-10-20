37e3bcfaeef180789914ee2240963de76bb8ab3a
Jérémy <jeremy.lixandre@gmail.com>
2019-10-20 21:22:36 +0200

    chg: :wrench: add node-sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import 'bulma/css/bulma.css';
-import 'bulma-helpers/css/bulma-helpers.min.css';
-import 'font-awesome/css/font-awesome.min.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Prototypes from './pages/Prototypes';
 import Profile from './pages/Profile';
-import './App.css';
+import './App.scss';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
@@ -16,7 +13,7 @@ function App() {
           <div id="page-container">
             <Router>
               <Navbar />
-              <div className="fill">        
+              <div className="fill">
                 <Route exact path="/" component={Home}/>
                 <Route path="/prototypes" component={Prototypes}/>
                 <Route path="/profil/u/:username" component={Profile}/>
