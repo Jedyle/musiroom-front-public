86104e50ba3fa1bfa2c7c457a72c09ee71911e60
Jérémy <jeremy.lixandre@gmail.com>
2022-04-19 16:44:04 +0200

    chg: :lipstick: add link to come back to profile from settings page

--- src/pages/Profile/Settings/index.js
+++ src/pages/Profile/Settings/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PublicProfileForm from './PublicProfile';
 import PasswordForm from './Password';
 import AvatarForm from './Avatar';
 import DeleteUserForm from './DeleteUser';
 import Title from 'components/Utils/Title';
 
+import { profileUrl } from 'pages/urls';
+
 const SettingsForm = (props) => (
     <div>
       <Title title="Settings"/>
+      <Link to={profileUrl(props.username)}>{"<"} Back to account</Link>
       <PublicProfileForm {...props} />
       <hr/>
       <AvatarForm />
