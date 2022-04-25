b0da90df0ccf9f2941afa104196af89afb68bba6
Jérémy <jeremy.lixandre@gmail.com>
2022-04-25 14:26:48 +0200

    chg: :construction: temporarily remove logo with text (until I have another one)

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -3,8 +3,9 @@ import './index.css';
 
 const Logo = () => (
     <div className="logo">
-        <img className="is-hidden-tablet" src="/logo_crop_black.png" />
-        <img className="is-hidden-mobile" src="/logo_black.png" />
+      <img className="is-hidden-tablet" src="/logo_crop_black.png" alt="logo" />
+      {/* temporary : for non mobile, add long logo when we have a working one */}
+      <img className="is-hidden-mobile" src="/logo_crop_black.png" alt="logo" />
     </div>
 );
 
