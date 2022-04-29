9049aa4e3accb0f3bb2adb84f048596b1ad276b9
Jérémy <jeremy.lixandre@gmail.com>
2022-04-30 01:34:00 +0200

    fix: :lipstick: typo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -37,7 +37,7 @@ const Home = () => {
               <div className="column is-12-mobile is-one-third-tablet has-background-light has-margin-right-1">
                 <p className="title is-size-4 has-text-centered">
                   <span className="icon"><i className="fa fa-star"></i></span>
-                  {"  "} Last ratings {"  "}
+                  {"  "} Ratings {"  "}
                   <span className="icon"><i className="fa fa-star"></i></span>
                 </p>
                 <hr style={{backgroundColor: 'black'}}/>
@@ -46,7 +46,7 @@ const Home = () => {
               <div className="column is-12-mobile is-one-third-tablet has-background-grey-lighter">
                 <p className="title is-size-4 has-text-centered">
                   <span className="icon"><i className="fa fa-music"></i></span>
-                  {"  "} Last reviews {"  "}
+                  {"  "} Reviews {"  "}
                   <span className="icon"><i className="fa fa-music"></i></span>
                 </p>
                 <hr style={{backgroundColor: 'black'}}/>
@@ -55,7 +55,7 @@ const Home = () => {
               <div className="column is-12-mobile is-one-third-tablet has-background-light">
                 <p className="title is-size-4 has-text-centered">
                   <span className="icon"><i className="fa fa-comment"></i></span>
-                  {"  "} Last comments {"  "}
+                  {"  "} Comments {"  "}
                   <span className="icon"><i className="fa fa-comment"></i></span>
                 </p>
                 <hr style={{backgroundColor: 'black'}}/>
